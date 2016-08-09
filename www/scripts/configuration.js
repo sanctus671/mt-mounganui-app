94f874d3789a0f5eea9ebb47022d482874c755bb
sanctus671 <taylor@taylorhamling.com>
2016-08-09 13:35:22 +1200

    .

--- www/scripts/configuration.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('ENV', {name:'development',apiUrl:'http://www.mountmaunganui.net.nz/api/',dataProvider:'CUSTOM',firebaseUrl:'https://business-directory.firebaseio.com/',youtubeKey:'AIzaSyDael5MmCQa1GKQNKQYypmBeB08GATgSEo',ionicPrivateKey:'45ae279804775982ccc0123239602403cf4413f6196257dc',ionicPublicKey:'13b0cd4631ef68fabcebec27b642b3d11ecb62f52289590a',ionicAppId:'fcfec3cc',gcmId:'228071472080'})
-
-;
